ba20430d1e0bfa601a01e568c04302130fdf19fb
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-05-04 16:42:22 -0700

    f4

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -2,7 +2,8 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        Dim l1 As Boolean = True
+        Dim o1 As Integer = 1
     End Sub
 
 End Class
