08b7ad300dc2de4c7321abf5a5a50c1421a67398
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-05-05 13:25:38 -0700

    Added MasterSub1()

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -3,9 +3,14 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         MasterSub()
+        MasterSub1()
     End Sub
 
     Private Sub MasterSub()
+        Return
+    End Sub
+
+    Private Sub MasterSub1()
         Return
     End Sub
 End Class
