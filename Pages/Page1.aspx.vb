66c233506db9b7ca548abc9145c6cd53d9d20d80
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-05-05 13:14:41 -0700

    Added MasterSub()

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -2,8 +2,10 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Dim l1 As Boolean = True
-        Dim o1 As Integer = 1
+        MasterSub()
     End Sub
 
+    Private Sub MasterSub()
+        Return
+    End Sub
 End Class
