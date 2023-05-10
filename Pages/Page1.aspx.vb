ecaab160f938af20069d2a9ed964c8c15319464c
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-05-10 14:00:54 -0700

    Start New Master

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -2,17 +2,6 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        MasterSub()
-        MasterSub1()
     End Sub
 
-    Private Sub MasterSub()
-        Return
-    End Sub
-
-    Private Sub MasterSub1()
-        'modified MasterSub1 - A
-        'modified MasterSub1 - B
-        Return
-    End Sub
 End Class
