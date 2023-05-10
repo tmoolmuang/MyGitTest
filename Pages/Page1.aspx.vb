b25c510bd56df4fd67604e308e900f49019a5c27
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-05-10 15:15:44 -0700

    another new message

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -4,6 +4,7 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         'comment A
         'comment B
+        'comment C
     End Sub
 
 End Class
