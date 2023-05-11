d49467806b876ce6a6bf458d479c11067d939bdd
Zyber777 <133165173+Zyber777@users.noreply.github.com>
2023-05-10 20:41:55 -0700

    RR 1234 -  some report changes

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -2,6 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim q As Integer = 2
     End Sub
 
 End Class
