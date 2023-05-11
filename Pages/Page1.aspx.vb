9165e77c4db635ce88b5eb026c3bb2f282cdea20
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-05-10 20:38:33 -0700

    start at master

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -2,13 +2,6 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        'Zyber added a comment
-        Dim a As Integer = 1
     End Sub
-
-    Private Function func1() As Boolean
-        Dim q As Integer = 2
-        Return True
-    End Function
 
 End Class
