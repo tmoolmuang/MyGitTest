192ba6f8f58d12c026bd77dad758c0d8377409fb
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-05-10 19:50:08 -0700

    my local commit 3

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -2,7 +2,8 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        'add some comment
+        callSub1()
     End Sub
 
     Private Sub callSub1()
