0efe8179b2d4fe3a0944bfec1ca7b857e841c3e7
Zyber777 <133165173+Zyber777@users.noreply.github.com>
2023-05-15 13:21:44 -0700

    z added comment

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        dim q = 1        
     End Sub
 
 End Class
