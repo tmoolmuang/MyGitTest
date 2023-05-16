561ab7198eb52ca1eb97ad75eade167870340bfc
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-05-15 20:32:54 -0700

    master head

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -2,15 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        dim q = 1        
+
     End Sub
 
-    Private Function z1() As Boolean
-        Return True
-    End Function
-
-    Private Function z2() As Boolean
-        Return True
-    End Function
-    
 End Class
