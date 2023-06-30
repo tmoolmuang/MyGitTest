553a38e7443b84a45b9bb3d71dca46ac7c066c91
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-06-30 14:59:50 -0700

    B

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -4,14 +4,5 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
-    Private Function T1() As Boolean
-        Return True
-    End Function
-    Private Function T2() As Boolean
-        Return True
-    End Function
-    Private Function T3() As Boolean
-        Return True
-    End Function
 
 End Class
