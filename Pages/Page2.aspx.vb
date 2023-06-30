feec1804cebc208200e2af425e360f2418c952fb
Zyber777 <133165173+Zyber777@users.noreply.github.com>
2023-06-30 08:46:52 -0700

    Z add Z1

--- Pages/Page2.aspx.vb
+++ Pages/Page2.aspx.vb
@@ -4,5 +4,8 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
-
+    
+    Private Function Z1() As Boolean
+        Return True
+    End Function
 End Class
