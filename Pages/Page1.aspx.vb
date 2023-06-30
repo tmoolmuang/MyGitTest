5816b362f216e0f3b16e7ce3e4e33e505826f210
Thanya Moolmuang <tmoolmuang@outlook.com>
2023-06-30 11:46:08 -0700

    combination of 3

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -4,5 +4,14 @@
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
     End Sub
+    Private Function T1() As Boolean
+        Return True
+    End Function
+    Private Function T2() As Boolean
+        Return True
+    End Function
+    Private Function T3() As Boolean
+        Return True
+    End Function
 
 End Class
