c8e4bb932f2a96d83372300ada12847022e05cb6
tmoolmuang <tmoolmuang@yahoo.com>
2024-01-16 14:21:49 -0800

    updated comment on Page1.vb

--- Pages/Page1.aspx.vb
+++ Pages/Page1.aspx.vb
@@ -3,6 +3,7 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         'can add some more comment here
+        'This line will be a starting point of test1 branch
     End Sub
 
 End Class
